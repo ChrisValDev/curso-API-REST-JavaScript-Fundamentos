b3f0c294e0cd2c0e730fcb829265071e898d34bc
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-11-12 13:54:31 -0600

    Endpoints y query parameters

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const URL_API = 'https://api.thecatapi.com/v1/images/search?limit=4';
 
 // fetch(URL)
 //     .then(res => res.json())
@@ -8,10 +8,20 @@ const URL = 'https://api.thecatapi.com/v1/images/search';
 //     });
 
 async function getCat() {
-    const res = await fetch(URL);
+    const res = await fetch(URL_API);
     const data = await res.json();
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+
+    console.log('data', data);
+
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    const img4 = document.getElementById('img4');
+    
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+    img4.src = data[3].url;
 }
 
 const boton = document.querySelector('button');
