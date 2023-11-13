0cb738148ed14f449d71bf7817c812955d68049c
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-11-13 17:42:15 -0600

    Consult API for write dynamic HTML

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=l
 const API_FAVORITES = 'https://api.thecatapi.com/v1/favourites?&api_key=live_G2Mo7qyg759KUXFkALRfIwZaixR8AWO25m6XgCLpgf37bNJL5Gb2QVgfxXMLBAUA';
 const API_KEY = 'live_G2Mo7qyg759KUXFkALRfIwZaixR8AWO25m6XgCLpgf37bNJL5Gb2QVgfxXMLBAUA';
 
-let spanError = document.getElementById("orto");
+const spanError = document.getElementById("orto");
 
 
 async function loadRandomMichis() {
@@ -16,9 +16,15 @@ async function loadRandomMichis() {
     } else {
         const img1 = document.getElementById('img1');
         const img2 = document.getElementById('img2');
+        const btn1 = document.getElementById('btn1');
+        const btn2 = document.getElementById('btn2');
         
         img1.src = data[0].url;
         img2.src = data[1].url;
+
+        btn1.onclick = () => saveFavoriteMichi(data[0].id);
+        btn2.onclick = () => saveFavoriteMichi(data[1].id);
+
     }
 }
 
@@ -30,25 +36,41 @@ async function loadFavoriteMichis() {
 
     if (res.status !== 200) {
         spanError.innerHTML = 'Hubo un error: ' + res.status + data.message;
+    } else {
+        data.forEach(michi => {
+            const section = document.getElementById('favoriteMichis');
+            const article = document.createElement('article');
+            const img = document.createElement('img');
+            const btn = document.createElement('button');
+            const btnText = document.createTextNode('Sacar michi de favoritos');
+
+            btn.appendChild(btnText);
+            img.src = michi.image.url;
+            img.width = 300;
+            article.appendChild(btn);
+            article.appendChild(img);
+            section.appendChild(article);
+
+        });
     }
 }
 
-async function saveFavoriteMichis() {
+async function saveFavoriteMichi(id) {
     const res = await fetch(API_FAVORITES, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            image_id: "e5k"
+            image_id: id
         }),
     });
-    // const data = await res.json();
+    const data = await res.json();
 
     console.log('Save')
     console.log(res)
 
-    if (res.status == 200) {
+    if (res.status !== 200) {
         spanError.innerHTML = 'Hubo un error: ' + res.status + data.message;
     }
 }
